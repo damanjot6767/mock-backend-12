d42a87b215f497dfea04aa6de24e725ec7ee068f
damanjot6767 <damanjot6767@gmail.com>
2023-01-21 15:10:55 +0530

    add singnup login and user profilte and calculate route

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,6 +4,9 @@ const UserSchema = new Schema({
    name:{type:String,required:true},
    email:{type:String,required:true,unique:true},
    password:{type:String,required:true,unique:true},
+   Maturity:{type:Number,default:0},
+   TotalInvest:{type:Number,default:0},
+   InterestGain:{type:Number,default:0}
 })
 
 const UserModel = new model("auth",UserSchema);
--- src/routes/users.js
+++ src/routes/users.js
@@ -17,4 +17,32 @@ UserDetails.get('/:id',async function(req, res) {
     } 
 });
 
+//Calculate
+UserDetails.post('/:id',async function(req, res) {
+    let {id} = req.params;
+    let {Instalment,InterestRate,Years}=req.body;
+    Instalment= Number(Instalment);
+    InterestRate=Number(InterestRate)
+    Years=Number(Years)
+    try{
+        let Maturity=0;
+        let TotalInvest=Years*Instalment;
+       for(let i=1;i<=Years;i++){
+        if(i==1){
+            Maturity+=Instalment*(1+(0.071*(1)))
+        }
+        else{
+            Maturity=(Maturity+Instalment)*(1+(0.071*(360/360)))
+        }
+       }
+       let InterestGain=Maturity-TotalInvest;
+        await UserModel.findByIdAndUpdate({_id:id},{Maturity,InterestGain,TotalInvest});
+        let ans = await UserModel.findOne({_id:id})
+       res.send(ans)
+    }
+    catch(err){
+        res.send(err.message);
+        return;
+    } 
+});
 module.exports = UserDetails;
